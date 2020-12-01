f01730c5d057f51083352fd9857f8c7f8e82253e
datpt11 <tiendat.phung11@gmail.com>
2020-12-01 23:10:34 +0700

    demo2

---
+++ src/components/Demo2/Demo2.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import styles from './Demo2.module.scss';
+
+export interface Demo2Props {}
+
+const Demo2: FC<Demo2Props> = () => {
+  return <div className={styles.container}>Demo2</div>;
+};
+
+export default Demo2;
