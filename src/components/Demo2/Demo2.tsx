fc651dd0e83d637cd6f93fb135a2a1ad3a8e3c35
datpt11 <tiendat.phung11@gmail.com>
2020-12-01 23:14:26 +0700

    demo3

--- src/components/Demo2/Demo2.tsx
+++ src/components/Demo2/Demo2.tsx
@@ -4,7 +4,7 @@ import styles from './Demo2.module.scss';
 export interface Demo2Props {}
 
 const Demo2: FC<Demo2Props> = () => {
-  return <div className={styles.container}>Demo2</div>;
+  return <div className={styles.container}>Demo5</div>;
 };
 
 export default Demo2;
