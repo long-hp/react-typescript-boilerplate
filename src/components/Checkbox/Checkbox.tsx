69a1e5ed368e54303b989e0317635b3add8a9f05
datpt11 <tiendat.phung11@gmail.com>
2020-12-05 15:47:45 +0700

    Checkbox Component

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,36 +1,34 @@
-import React, { FC, InputHTMLAttributes, useState } from 'react';
-import { Size, Text, WithStylesProps } from 'wiloke-react-core';
+import React, { FC, InputHTMLAttributes, memo, ReactNode, useEffect, useState } from 'react';
+import { Size, Text, withTachyons, WithTachyonsProps } from 'wiloke-react-core';
 import { classNames } from 'wiloke-react-core/utils';
 import styles from './Checkbox.module.scss';
 
-export interface CheckboxProps extends WithStylesProps {
+export interface CheckboxProps extends WithTachyonsProps {
   /** Kich thuoc checkbox */
   size?: Size;
   /** Trang thai cua checkbox */
   checked?: boolean;
   /** Trang thai default cua checkbox */
   defaultChecked?: boolean;
-  /** Trang thai khong duoc check */
-  indeterminate?: boolean;
   /** Disabled Checkbox */
   disabled?: boolean;
+  children?: ReactNode;
   /** Su kien onChange */
   onChange?: InputHTMLAttributes<HTMLInputElement>['onChange'];
 }
 
-const Checkbox: FC<CheckboxProps> = ({
+const CheckboxComponent: FC<CheckboxProps> = ({
   size = 'small',
-  checked = true,
-  defaultChecked = true,
-  disabled,
-  indeterminate,
+  checked = false,
+  defaultChecked = false,
+  disabled = false,
   children,
   className,
   onChange,
   ...rest
 }) => {
   const [checkedState, setCheckedState] = useState(defaultChecked);
-  const checkedClass = checkedState ? styles.check : '';
+  const checkedClass = checkedState ? styles.checked : '';
   const sizeClass = styles[size];
   const disabledClass = disabled ? styles.disabled : '';
   const classes = classNames(styles.container, disabledClass, sizeClass, className);
@@ -42,6 +40,12 @@ const Checkbox: FC<CheckboxProps> = ({
     setCheckedState(!checkedState);
     onChange?.(e);
   };
+
+  useEffect(() => {
+    if (checked) {
+      setCheckedState(!!checked);
+    }
+  }, [checked]);
   return (
     <Text {...rest} tagName="label" className={classes} tachyons={['flex', 'items-center']}>
       <Text tagName="span" className={classNames(styles.checkboxInput, sizeClass, checkedClass, disabledClass)}>
@@ -53,4 +57,6 @@ const Checkbox: FC<CheckboxProps> = ({
   );
 };
 
-export default Checkbox;
+const Checkbox = withTachyons<HTMLElement, CheckboxProps>(CheckboxComponent);
+
+export default memo(Checkbox);
