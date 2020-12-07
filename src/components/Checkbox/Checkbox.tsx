7c0c050fb07bd69ca946a6a5363e942fafa2e1ab
datpt11 <tiendat.phung11@gmail.com>
2020-12-07 15:59:01 +0700

    fix style, storybook Checkbox component#2

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,10 +1,10 @@
 import React, { FC, InputHTMLAttributes, ReactNode, useEffect, useState } from 'react';
-import { Size, Text, WithTachyonsProps } from 'wiloke-react-core';
+import { ColorNames, LineAwesome, Size, Text } from 'wiloke-react-core';
 import { classNames, memoization } from 'wiloke-react-core/utils';
 import styles from './Checkbox.module.scss';
 import CheckboxLoading from './CheckboxLoading';
 
-export interface CheckboxProps extends WithTachyonsProps {
+export interface CheckboxProps {
   /** Kich thuoc checkbox */
   size?: Size;
   /** Trang thai cua checkbox */
@@ -13,21 +13,45 @@ export interface CheckboxProps extends WithTachyonsProps {
   defaultChecked?: boolean;
   /** Disabled Checkbox */
   disabled?: boolean;
-  children?: ReactNode;
+  /** Backgroundcolor checkbox*/
+  color?: ColorNames;
+  /** Color icon ben trong checkbox */
+  colorIcon?: ColorNames;
+  /** Classname */
+  className?: string;
+  /** Icon ben trong checkbox */
+  Icon?: ReactNode;
   /** Su kien onChange */
   onChange?: InputHTMLAttributes<HTMLInputElement>['onChange'];
 }
 
-interface CheckboxFC extends FC<CheckboxProps> {
+const Checkbox: FC<CheckboxProps> & {
   Loading: typeof CheckboxLoading;
-}
-
-const Checkbox: CheckboxFC = ({ size = 'medium', checked, defaultChecked = false, disabled = false, children, className, onChange, ...rest }) => {
+} = ({
+  size = 'extra-small',
+  checked,
+  defaultChecked = false,
+  disabled = false,
+  children,
+  className,
+  Icon,
+  color = 'behance',
+  colorIcon = 'light',
+  onChange,
+  ...rest
+}) => {
   const [checkedState, setCheckedState] = useState(defaultChecked);
   const checkedClass = checkedState ? styles.checked : '';
   const sizeClass = styles[size];
   const disabledClass = disabled ? styles.disabled : '';
   const classes = classNames(styles.container, disabledClass, sizeClass, className);
+
+  const defaultIconMapping: Record<Size, ReactNode> = {
+    'extra-small': <LineAwesome name="check" size={12} />,
+    small: <LineAwesome name="check" size={16} />,
+    medium: <LineAwesome name="check" size={20} />,
+    large: <LineAwesome name="check" size={24} />,
+  };
 
   const _handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (disabled) {
@@ -47,7 +71,13 @@ const Checkbox: CheckboxFC = ({ size = 'medium', checked, defaultChecked = false
     <Text {...rest} tagName="label" className={classes} tachyons={['inline-flex', 'items-center']}>
       <Text tagName="span" className={classNames(styles.checkbox, sizeClass, checkedClass, disabledClass)}>
         <input disabled={disabled} className={styles.checkboxInput} checked={checkedState} type="checkbox" onChange={_handleChange} />
-        <Text tagName="span" className={styles.control}></Text>
+        <Text tagName="span" backgroundColor={checkedState ? color : 'light'} className={styles.control}>
+          {checkedState && (
+            <Text className={styles.icon} color={colorIcon} tagName="span">
+              {Icon || defaultIconMapping[size]}
+            </Text>
+          )}
+        </Text>
       </Text>
       {children && <Text tagName="span">{children}</Text>}
     </Text>
--- src/stories/3-Components/Checkbox.stories.tsx
+++ src/stories/3-Components/Checkbox.stories.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Checkbox from 'components/Checkbox';
 import { boolean, select, text } from '@storybook/addon-knobs';
-import { Size } from 'wiloke-react-core';
+import { defaultColors, Size } from 'wiloke-react-core';
 import getOptions from 'stories/utils/getOptions';
 import { action } from '@storybook/addon-actions';
 
@@ -15,6 +15,8 @@ export const WithProps = () => {
   let size;
   let checked;
   let disabled;
+  let colorIcon;
+  let color;
   if (!isLoading) {
     checked = boolean('Checked', false);
     disabled = boolean('Disabled', false);
@@ -23,6 +25,8 @@ export const WithProps = () => {
       getOptions<Size[]>(['extra-small', 'small', 'medium', 'large']),
       'medium',
     ) as any;
+    colorIcon = select('Color Icon', getOptions(defaultColors), 'light') as any;
+    color = select('Color Checkbox', getOptions(defaultColors), 'behance') as any;
   }
   const [checkedState, setCheckedState] = useState(checked);
 
@@ -34,7 +38,7 @@ export const WithProps = () => {
   return isLoading ? (
     <Checkbox.Loading />
   ) : (
-    <Checkbox disabled={disabled} size={size} checked={checked} onChange={_handleChange}>
+    <Checkbox color={color} colorIcon={colorIcon} disabled={disabled} size={size} checked={checked} onChange={_handleChange}>
       {text('Children', 'Day la checkbox')}
     </Checkbox>
   );
