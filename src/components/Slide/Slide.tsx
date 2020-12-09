215131b01f720a2d5ad4f9543aedb48a07d89eba
datpt11 <tiendat.phung11@gmail.com>
2020-12-09 22:30:50 +0700

    Slider Show #2

--- src/components/Slide/Slide.tsx
+++ src/components/Slide/Slide.tsx
@@ -1,11 +1,17 @@
-import React, { FC } from 'react';
+import React, { forwardRef } from 'react';
 import { View } from 'wiloke-react-core';
 import styles from './Slide.module.scss';
 
-export interface SlideProps {}
+export interface SlideProps {
+  title: string;
+}
 
-const Slide: FC<SlideProps> = () => {
-  return <View className={styles.container}></View>;
-};
+const Slide = forwardRef<HTMLDivElement, SlideProps>(({ title }: SlideProps, ref: any) => {
+  return (
+    <View ref={ref} className={styles.container}>
+      {title}
+    </View>
+  );
+});
 
 export default Slide;
--- src/components/SliderShow/SliderShow.tsx
+++ src/components/SliderShow/SliderShow.tsx
@@ -1,6 +1,6 @@
 import Slide from 'components/Slide/Slide';
-import React, { FC, useEffect, useRef } from 'react';
-import { View } from 'wiloke-react-core';
+import React, { FC, useEffect, useRef, useState } from 'react';
+import { LineAwesome, Text, View } from 'wiloke-react-core';
 import styles from './SliderShow.module.scss';
 
 export interface Slide {
@@ -11,27 +11,53 @@ export interface SliderShowProps {
   items?: Slide[];
 }
 
-const SliderShow: FC<SliderShowProps> = ({}) => {
+const SliderShow: FC<SliderShowProps> = ({ items = [{ title: '1' }, { title: '2' }, { title: '3' }] }) => {
+  const [indexActive, setIndexActive] = useState(1);
   const containerRef = useRef<HTMLDivElement | null>(null);
   const innerRef = useRef<HTMLDivElement | null>(null);
+  const itemRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    if (innerRef.current !== null) {
-      innerRef.current.style.flexDirection = 'column';
+    if (innerRef.current !== null && itemRef.current !== null) {
+      itemRef.current.style.width = `350px`;
     }
-  }, [innerRef]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
+  useEffect(() => {
+    if (containerRef.current !== null && innerRef.current !== null) {
+      console.log(indexActive * containerRef.current.offsetWidth);
+      innerRef.current.style.width = `${indexActive * containerRef.current.offsetWidth}`;
+      console.log((indexActive * containerRef.current.offsetWidth) / items?.length);
+      innerRef.current.style.transform = `translateX(-${(indexActive * containerRef.current.offsetWidth) / items?.length})`;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [indexActive]);
+
+  const _handleSlideNext = () => {
+    setIndexActive(indexActive => Math.min(indexActive + 1, items.length - 1));
+  };
+
+  const _handleSlidePrev = () => {
+    setIndexActive(indexActive => Math.max(indexActive - 1, 0));
+  };
+  console.log(indexActive);
   return (
     <View ref={containerRef} className={styles.container}>
       <View ref={innerRef} className={styles.wrapper}>
-        <Slide />
-        <Slide />
-        <Slide />
-        <Slide />
-        <Slide />
-        <Slide />
-        <Slide />
-        <Slide />
+        {items.map((item: Slide, index: number) => {
+          return <Slide ref={itemRef} key={index} title={item.title} />;
+        })}
+      </View>
+      <View onClick={_handleSlideNext} className={styles.btnNext}>
+        <Text tagName="span" className={styles.iconNext}>
+          <LineAwesome name="arrow-right"></LineAwesome>
+        </Text>
+      </View>
+      <View onClick={_handleSlidePrev} className={styles.btnPrev}>
+        <Text tagName="span" className={styles.iconPrev}>
+          <LineAwesome name="arrow-left"></LineAwesome>
+        </Text>
       </View>
     </View>
   );
