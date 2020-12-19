26918fdd0767c396d8bd20d540191cd10b684d8f
wiloke1 <hpvanlong@gmail.com>
2020-12-19 22:13:10 +0700

    update wiloke-react-core 0.4.2

--- src/components/PostCard/PostCardLoading.tsx
+++ src/components/PostCard/PostCardLoading.tsx
@@ -7,7 +7,7 @@ const PostCardLoading: FC<PostCardLoadingProps> = () => {
   return (
     <View backgroundColor="light" radius="round" tachyons={['relative', 'overflow-hidden']}>
       <View backgroundColor="gray4" aspectRatioInPercent={56.25} />
-      <View backgroundColor="gray5" opacity="60/100" height={32} width={80} radius={4} tachyons={['absolute', 'top-1', 'left-1', 'ph3', 'pv1']} />
+      <View backgroundColor="gray5" wilokeStyles="o-60" height={32} width={80} radius={4} tachyons={['absolute', 'top-1', 'left-1', 'ph3', 'pv1']} />
       <View tachyons={['pa3', 'flex', 'justify-between', 'items-center']} height={52}>
         <View backgroundColor="gray4" radius={4} height={10} tachyons={['w-70', 'nowrap', 'f6']} />
         <View backgroundColor="gray4" radius={4} height={20} width={20} tachyons={['nowrap', 'f6']} />
