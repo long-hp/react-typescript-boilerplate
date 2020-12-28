56b1565cc9491b704138362020be90e04fd0d9ee
wiloke1 <hpvanlong@gmail.com>
2020-12-28 16:05:33 +0700

    update README + storybook

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,6 +2,6 @@ import { create } from '@storybook/theming/create';
 
 export default create({
   base: 'light',
-  brandTitle: 'Drawtool',
+  brandTitle: 'Wiloke',
   brandImage: 'https://placehold.it/350x150',
 });
